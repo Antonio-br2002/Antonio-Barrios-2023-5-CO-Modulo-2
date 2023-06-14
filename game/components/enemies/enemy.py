ecc12745200d99d681e094f11db9a086c77e2f84
Antonio-br2002 <136205090+Antonio-br2002@users.noreply.github.com>
2023-06-14 17:26:38 -0500

    Second-Class-Homework

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 from pygame.sprite import Sprite
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH, ENEMY_2
 
 class Enemy(Sprite):
     ENEMY_WIDTH = 46
@@ -12,9 +12,9 @@ class Enemy(Sprite):
     SPEED_X = 5
     SPEED_y = 1
     MOV_X = { 0: 'left', 1: 'right'}
-    
-    def __init__(self):
-        self.image = pygame.transform.scale(ENEMY_1, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
+
+    def __init__(self, image):
+        self.image = pygame.transform.scale(image, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS_LIST[random.randint(0, 10)]
         self.rect.y = self.Y_POS
@@ -23,31 +23,30 @@ class Enemy(Sprite):
         self.movement_x = self.MOV_X[random.randint(0, 1)]
         self.move_x_for = random.randint(30, 100)
         self.index = 0
-    
+
     def update(self, ships):
         self.rect.y += self.speed_y
-        
+
         if self.movement_x == 'left':
             self.rect.x -= self.speed_x
             self.change_movement_x()
         else:
             self.rect.x += self.speed_x
             self.change_movement_x()
-        
+
         if self.rect.y >= SCREEN_HEIGHT:
             ships.remove(self)
-        
+
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-        
+
+
+
     def change_movement_x(self):
         self.index += 1
-        if(self.index >= self.move_x_for and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH - self.ENEMY_WIDTH):
+        if (self.index >= self.move_x_for and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH - self.ENEMY_WIDTH):
             self.movement_x = 'left'
             self.index = 0
-        elif(self.index >= self.move_x_for and self.movement_x == 'left') or (self.rect.x <= 10):
-          self.movement_x = 'right'
-          self.index = 0
-        
-          
-        
+        elif (self.index >= self.move_x_for and self.movement_x == 'left') or (self.rect.x <= 10):
+            self.movement_x = 'right'
+            self.index = 0
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,20 +1,24 @@
 from game.components.enemies.enemy import Enemy
+from game.utils.constants import ENEMY_1, ENEMY_2
+
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
-        
+
     def update(self):
       self.add_enemy()
-        
+
       for enemy in self.enemies:
         enemy.update(self.enemies)
-        
+
     def draw(self, screen):
       for enemy in self.enemies:
           enemy.draw(screen)
-            
+
     def add_enemy(self):
-      if len(self.enemies) < 1:
-        enemy = Enemy()
-        self.enemies.append(enemy)
+      if len(self.enemies) < 2:
+            enemy1 = Enemy(ENEMY_1)
+            enemy2 = Enemy(ENEMY_2)
+            self.enemies.append(enemy1)
+            self.enemies.append(enemy2)
